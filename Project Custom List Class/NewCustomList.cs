5e62b9f6fab8886c7ec55f2f8e3b995781dfb2b7
unknown <theusjt49@gmail.com>
2020-01-24 07:50:27 -0600

    created an indexer in the new custom lists class and added a constructor with defined values of count equaling zero and items equaling new items.

--- Project Custom List Class/NewCustomList.cs
+++ Project Custom List Class/NewCustomList.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Project_Custom_List_Class
 {
-    class Custom_List
+    public class NewCustomList<T>
     {
+        
+        public T[] items;
+        public int capacity;
+        public int count;
+
+        public NewCustomList()
+        {
+            count = 0;
+            items = new T[4];
+        }
+        public void Add(T item)
+        {
+            items[count] = item;
+            if (count == capacity)
+            {
+                capacity *= 2; 
+            }
+            count++;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+
     }
+
+   
+    
+
 }
--- Project Custom List Test/Custom List Test.cs
+++ Project Custom List Test/Custom List Test.cs
@@ -8,9 +8,110 @@ namespace Project_Custom_List_Test
     public class CustomListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_IndexZero_CheckifAddfillsIndexZero()
         {
+            //Arrange
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int expected = 4;
+            int actual;
 
+            //Act
+            mylist.Add(4);
+            actual = mylist[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+        public void Add_IndexOne_CheckifAddCountiuestraisecount()
+        {
+            //Arrange
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int zeroValue = 4;
+            int expected = 5;
+            int actual;
+
+            //Act
+            mylist.Add(5);
+            actual = mylist[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_IndexTwo_CheckifAddfiilstheTwoIndex()
+        {
+            //Array
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int zeroValue = 4;
+            int oneValue = 5;
+            int expected = 6;
+            int actual;
+
+            //Act
+            mylist.Add(6);
+            actual = mylist[2];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_IndexThree_CheckIfAddFiilstheThreeIndex()
+        {
+            //Array
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int zeroValue = 4;
+            int oneVealue = 5;
+            int twoValue = 6;
+            int expected = 7;
+            int actual;
+
+            //Act
+            mylist.Add(7);
+            actual = mylist[3];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_IndexFour_CheckifAddfiilstheFourIndex()
+        {
+            //Array
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int zeroValue = 4;
+            int oneVealue = 5;
+            int twoValue = 6;
+            int threeValue = 7;
+            int expected = 8;
+            int actual;
+
+            //Act
+            mylist.Add(8);
+            actual = mylist[4];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_Capacitydoubling_AddShouldDoubleCapacityAfterArrayIsFilled()
+        {
+            //Array
+            NewCustomList<int> mylist = new NewCustomList<int>();
+            int zerovalue= 4;
+            int oneValue = 5;
+            int twoalue = 6;
+            int threeValue = 7;
+            int fourValue = 8;
+            int expected = 9;
+            int actual;
+
+
+            //Act
+            mylist.Add(9);
+            actual = mylist[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual); 
+        } 
+
     }
 }
